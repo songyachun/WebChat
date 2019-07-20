e7d5b1e02c8368823c37a81ad4672aed57d78602
Tedu <fhw@163.com>
2019-07-20 20:20:19 +0800

    添加user_user,user_info表

---
+++ database/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Database1Config(AppConfig):
+    name = 'database'
--- webchat/settings.py
+++ webchat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'database',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'webchat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webchat',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456',
     }
 }
 
