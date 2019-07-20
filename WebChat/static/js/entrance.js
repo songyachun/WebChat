06f10a730c314acc571ae984abd9eebcf52960f0
szmzaid1904 <yachun0124@163.com>
2019-07-20 23:08:24 +0800

    a

--- WebChat/static/js/entrance.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.login-content input').focus(function(){
-        $(this).prev().css('color','#999999');
-    })
-    $('.login-content input').blur(function(){
-        $(this).prev().css('color','#000000');
-    })
-    
-})
