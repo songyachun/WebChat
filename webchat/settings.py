1b5b5cdd3b8304cb5647c62cbef08ca7368aedfa
szmzaid1904 <yachun0124@163.com>
2019-07-21 21:09:59 +0800

    '倒入pymysql模块'

--- webchat/settings.py
+++ webchat/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'verify',
-    'database',
+    # 'database',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,"static"),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
 )
